47085bcdaee417ed20e905c6284ea3bb24cc92af
MoisesFloresv <85973188+MoisesFloresv@users.noreply.github.com>
2021-06-16 03:37:59 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,18 @@
 
 
+//Autor: Flores Ventura Moises
+
 //Esta es la parte donde ponermos la configuración de nuestra base de datos en Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB63VZApYCI65TwFn0gKLNzLrcS-g-4oRY",
-    authDomain: "proyectofinalweb50-fd251.firebaseapp.com",
-    databaseURL: "https://proyectofinalweb50-fd251-default-rtdb.firebaseio.com",
-    projectId: "proyectofinalweb50-fd251",
-    storageBucket: "proyectofinalweb50-fd251.appspot.com",
-    messagingSenderId: "27360742093",
-    appId: "1:27360742093:web:f9073fff24e5e9f2717784",
-    measurementId: "G-EGBVW9R2TQ"
+  authDomain: "proyectofinalweb50-fd251.firebaseapp.com",
+  databaseURL: "https://proyectofinalweb50-fd251-default-rtdb.firebaseio.com",
+  projectId: "proyectofinalweb50-fd251",
+  storageBucket: "proyectofinalweb50-fd251.appspot.com",
+  messagingSenderId: "27360742093",
+  appId: "1:27360742093:web:f9073fff24e5e9f2717784",
+  measurementId: "G-EGBVW9R2TQ"
 };
   // Aquí arrancamos nuestro Firebase
   firebase.initializeApp(firebaseConfig);
@@ -19,7 +22,7 @@ var db = firebase.firestore();
 
 let array1 = []
 
-    db.collection("Medicinas").get().then((querySnapshot) => {
+    db.collection("Productos").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
         array1.push(doc.data())
@@ -47,24 +50,24 @@ console.log(nombre)
 console.log(marca)
 console.log(cantidad)
 
-db.collection("Medicinas").add({
+db.collection("Productos").add({
     Nombre:nombre,
     Marca:marca,
     Cantidad:cantidad
 })
 .then(function(docRef) {
-    console.log("El medicamento se guardo correctamente en la base de datos con el ID: ", docRef.id);
+    console.log("Se guardó correctamente en la base de datos con el ID: ", docRef.id);
 })
 .catch(function(error) {
-    console.error("Error al guardar el medicamento: ", error);
+    console.error("Se produjo un error al guardar: ", error);
 });	
 }
 
 function eliminar(){
 	var id = document.getElementById("tercero").value
 
-	db.collection("Medicinas").doc(id).delete().then(function() {
-    console.log("registro se ha eliminado correctamente!");
+	db.collection("Productos").doc(id).delete().then(function() {
+    console.log("El registro se ha eliminado correctamente!");
 }).catch(function(error) {
     console.error("Error al eliminar un documento: ", error);
 });
@@ -75,7 +78,7 @@ function editar(){
 	var id = document.getElementById("cuarto").value
 
 	// To update age and favorite color:
-db.collection("Medicinas").doc(id).update({
+db.collection("Productos").doc(id).update({
     "CampoMarca": "Pziser",
     
 })
@@ -84,4 +87,4 @@ db.collection("Medicinas").doc(id).update({
 });
 
 }
-console.log("Cargado Correctamente") 
+console.log("Cargado Exitosamente")
